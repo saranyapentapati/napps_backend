5a5c6d845562b16fa0b9155aa86fec3bef61b47c
saranyapentapati <saranyapentapati16@gmail.com>
2025-05-19 12:25:33 +0530

    database

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
 # Replace with your MongoDB connection string
-MONGO_URL = "mongodb+srv://saranyapentapati16:aradhy%402516@cluster0.02n8dyi.mongodb.net/?retryWrites=true&w=majority&appName=cluster0"
+MONGO_URL = "mongodb+srv://saranyapentapati16:abcd%401234@cluster0.02n8dyi.mongodb.net/?retryWrites=true&w=majority&appName=cluster0"
 client = AsyncIOMotorClient(MONGO_URL)
 db = client.inventory_db  # database
 collection = db.items     # collection
